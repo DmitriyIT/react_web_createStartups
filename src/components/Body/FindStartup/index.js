3cb54306fb1614e48299b542e45b786d295a60df
Dmitriy Baranov <mit1035@yandex.ru>
2019-02-09 22:32:00 +0300

    add download startups to component

--- src/components/Body/FindStartup/index.js
+++ src/components/Body/FindStartup/index.js
@@ -1,18 +1,42 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import './FindStartup.css';
 
 import SearchJoinLine from '../SearchJoinLine';
 import LineOFConditions from '../LineOFConditions';
 import StartupCards from '../StartupCards';
 
-var FindStartup = (props) => {
-	return (
-		<Fragment>
-			<SearchJoinLine />
-			<LineOFConditions />
-			<StartupCards />
-		</Fragment>
-	);
-};
+
+class FindStartup extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			startups: [],
+			str_search: ''
+		}
+	}
+
+	componentDidMount() {
+		fetch('/startups') 
+			.then(response => response.json())
+			.then(data => this.onDownload(data));
+	}
+
+	onDownload(data) {
+		console.log(data);
+		this.setState({
+			startups: data
+		});
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<SearchJoinLine />
+				<LineOFConditions />
+				<StartupCards startups={this.state.startups} />
+			</Fragment>
+		);
+	}
+}
 
 export default FindStartup;
