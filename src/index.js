7b9cf599238fb20a402eafb403b0dd0c1f4dbee4
Dmitriy Baranov <mit1035@yandex.ru>
2019-02-15 17:47:37 +0300

    some chnges and rm trying dir

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import React from 'react';
 import {render} from 'react-dom';
 import Join from './components/Join.js';
 
-// var FinalComp = () => {
-// 	return (
-// 		<BrowserRouter>
-// 			<Switch>
-// 				Route
-// 			</Switch>
-// 		</BrowserRouter>
-// 	);
-// }
+
 
 render(<Join />, document.getElementById('this_for_react'));
