8d83f5b564568558cd31658707c11986160451a4
Dmitriy Baranov <mit1035@yandex.ru>
2019-04-10 00:03:32 +0300

    cards of startups change to new dis

--- src/components/Body/FindStartup/StartupCards/StartupCard/index.js
+++ src/components/Body/FindStartup/StartupCards/StartupCard/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './StartupCard.css';
+import './StartupCard.scss';
 import Startup__popup from './Startup__popup';
 
 var StartupCard = ({ title, body, id, more_info }) => {
 	return (
 		<div className="StartupCard">
-			<div className="StartupCard__topLine"/>
 			<div className="StartupCard__title"> {title} </div>
 			<div className="StartupCard__body"> {body} </div>
 			<div className="StartupCard__bottomLine"/>
+
 			<img className="StartupCard__triangle" src="../../img/triangle.svg" />
 			<div className="StartupCard__button">Просмотр</div>
 			<Startup__popup {...more_info} />
