dc32b3d716e19fae8a79ff77855d62aaceec0078
Dmitriy Baranov <mit1035@yandex.ru>
2019-05-01 18:46:32 +0300

    add members page

---
+++ src/components/Body/MyStartup/StartupMain/Members/Member/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Member.scss';
+
+var Member = ({ name, job, img_src, id, show_button, show_popup, click_showPopup, click_remove }) => {
+	var popupClassShow = show_popup ? '' : 'PopupRemove_show-no';
+	var button = show_button ? (
+		<div className="Member__buttonRemove">
+			<p onClick={click_showPopup} id={id}> Исключить </p>
+			<div className={"PopupRemove " + popupClassShow}>
+				<div className="PopupRemove__text">Вы уверены что хотите исключить участника  ?</div>
+				<div className="PopupRemove__buttonNo" 
+					onClick={click_showPopup} 
+					id={id}> Отмена </div>
+				<div className="PopupRemove__buttonRemove" 
+					onClick={click_remove} 
+					id={id}> Исключить </div>
+			</div>
+		</div>
+	) : '';
+
+	return (
+		<div className="Member">
+			<div className="Member__img">
+				<img src={img_src} />
+			</div>
+			<div className="Member__line"></div>
+			<div className="Member__info">
+				<div className="Member__info__name">{name}</div>
+				<div className="Member__info__job">{job}</div>
+			</div>
+			{button}
+		</div>
+	);
+}
+export default Member;
--- src/components/Body/MyStartup/StartupMain/Members/index.js
+++ src/components/Body/MyStartup/StartupMain/Members/index.js
@@ -1,14 +1,81 @@
 import React, { Component } from 'react';
 import './Members.scss';
 
+import Member from './Member';
+
 class Members extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			members: [],
+			show_button: false
+		}
+	}
 
+	componentDidMount() {
+		fetch('/getMembers')
+			.then(response => response.json())
+			.then(data => this.setState({
+				members: data.members,
+				show_button: data.isAdmin
+			}));
+	}
+
+	click_showPopup = (elem) => {
+		var id = elem.target.getAttribute('id');
+		var newStateMembers = this.state.members.map((member) => {
+			if (member.id == id) {
+				member.show_popup = !member.show_popup;
+			} else {
+				member.show_popup = false;
+			}
+			return member
+		});
+
+		this.setState({members: newStateMembers});
+	}
+
+	click_remove = (elem) => {
+		var id = elem.target.getAttribute('id');
+
+		fetch("/delMember", {
+		    headers: {
+		      'Accept': 'application/json',
+		      'Content-Type': 'application/json'
+		    },
+		    method: "POST",
+		    body: JSON.stringify({id: id})
+		})
+		.then((response) => response.json())
+		.then((data) => {
+			if (data.ans) {
+				var newMembersState = this.state.members.filter(e => e.id != id);
+				this.setState({members: newMembersState});
+			} else {
+				// err of server
+			}
+		})
+		.catch(res => console.log('catch' + res));
 	}
 
 	render() {
-		return <div> Members</div>;
+		var mbrs = this.state.members.map(e => (
+			<Member 
+				name={e.name} 
+				job={e.job} 
+				img_src={e.img_src} 
+				id={e.id} 
+				key={e.id} 
+				show_button={this.state.show_button} 
+				click_showPopup={this.click_showPopup} 
+				show_popup={e.show_popup}
+				click_remove={this.click_remove} />
+		));
+		return (
+			<div className="Members">
+				{mbrs}
+			</div>
+		);
 	}
 }
 
